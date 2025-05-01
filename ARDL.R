a3b406ef2c0609ff34a42ddb62ddbf62162cd213
Nabil <futbolnabil@hotmail.com>
2025-05-01 18:02:12 -0300

    ardl

---
+++ ARDL.R
@@ -0,0 +1,37 @@
+# pacotes
+library(tidymodels)
+library(glmnet)
+library(tidyverse)
+library(rbcb)
+library(quantmod)
+library(bestNormalize)
+library(gghighlight)
+library(corrplot)
+
+
+# Selecionando as variáveis -----------------------------------------------
+
+# inad cartão de crédito
+
+# inad Mercado de crédito
+
+# PIB
+
+# Taxa de Juros
+
+# Inflação
+
+# Volume Credito
+
+# Taxa de cambio 
+
+# Taxa de desemprego
+
+# Crise
+
+# Quantitadade de cartões ativos
+
+
+# Modelo ARDL -------------------------------------------------------------
+
+
